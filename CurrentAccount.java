3b984522466f13eed630bd27fd8fbb7695c6b960
Ahlas Irshad <ahlasjr11@gmail.com>
2025-12-31 22:05:18 +0530

    New update

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author HP
- */
 public class CurrentAccount extends BankAccount {
 
     private double overdraftLimit;
@@ -41,47 +32,3 @@ public class CurrentAccount extends BankAccount {
     }
     
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author HP
- */
-public class CurrentAccount extends BankAccount {
-
-    private double overdraftLimit;
-    private double transactionFee;
-
-    public CurrentAccount(String accountNumber, double balance,
-                          double overdraftLimit, double transactionFee) {
-        super(accountNumber, balance);
-        this.overdraftLimit = overdraftLimit;
-        this.transactionFee = transactionFee;
-    }
-    public CurrentAccount(String accountNumber, double balance){
-       super(accountNumber, balance); 
-    }
-    @Override
-    public boolean withdraw(double amount) {
-        if (getBalance() + overdraftLimit >= amount) {
-            setBalance(getBalance() - amount - transactionFee);
-        }
-        return true;
-    }
-
-    @Override
-    public void calculateInterest() {
-        // Current accounts usually have no interest
-        System.out.println("No interest for Current Account");
-    }
-
-    public double getOverdraftBalance() {
-        return overdraftLimit + getBalance();
-    }
-    
-}
->>>>>>> c0e59ef2047b8890d2bbd5253a5b09108b694b8a
